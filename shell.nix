68d011bdddea025734d3be9cfe2df5fb396078d8
Alejandro Gallo <aamsgallo@gmail.com>
2022-02-20 16:48:25 +0100

    Comment out clasp

--- shell.nix
+++ shell.nix
@@ -2,13 +2,14 @@
 
 pkgs.mkShell rec {
 
+  CMU_PATH = "${pkgs.cmucl_binary}/bin/lisp";
   buildInputs = with pkgs; [
     sbcl
     ccl
     clisp
     ecl
     gcl
-    clasp-common-lisp
+    # clasp-common-lisp
     cmucl_binary
     abcl
   ];
